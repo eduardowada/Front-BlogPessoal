04d751c461371e9e56fa64e87d5118915a753620
Eduardo Wada <156962925+eduardowada@users.noreply.github.com>
2024-08-18 23:05:21 -0300

    Fix files

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../contexts/AuthContext'
+import './Navbar.css';
+
+
+function Navbar() {
+  let navigate = useNavigate()
+
+    const { usuario, handleLogout } = useContext(AuthContext)
+
+    function logout() {
+        handleLogout()
+        alert('Usuário deslogado com sucesso')
+        navigate('/login')
+    }
+
+    let navbarComponent
+
+    if(usuario.token !== "") {
+      navbarComponent = (
+        <div className='w-full bg-indigo-800 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+            <Link to='/home' className='text-2xl font-bold uppercase title-custom'>TechBlog</Link>
+
+            <div className='flex gap-4'>
+              <Link to='/postagens' className='hover:underline'>Postagens</Link>
+              <Link to='/temas' className='hover:underline'>Temas</Link>
+              <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+              <Link to='/perfil' className='hover:underline'>Perfil</Link>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+  return (
+    <>
+      {navbarComponent}
+    </>
+  )
+}
+
+export default Navbar
